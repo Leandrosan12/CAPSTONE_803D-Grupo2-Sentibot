be2e60ce56ea47995d80bea970f8606caf8ad00d
ProSebast <seba.meli.alvarez@gmail.com>
2025-09-21 02:05:44 -0300

    Login, Registro, Aun no se hacen verificaciones "Imagen nueva"

--- Sentibot_Desarrollo/gestion/views.py
+++ Sentibot_Desarrollo/gestion/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+def registro(request):
+    return render(request, 'registro.html')
+def login(request):
+    return render(request, 'login.html')
+
+
+
+    
 def perfil(request):
-    return render(request, 'perfil.html') 
+    return render(request, 'perfil.html')
